9bfc3451e0365b3d42ce0c57541d70fc9ce64bb6
SandwichedAvocado <ngweiyuan8@gmail.com>
2023-06-10 16:41:29 +0800

    hej

--- dragdrop.js
+++ dragdrop.js
@@ -1,29 +1,30 @@
 var isResizing = false;
 
-(function() {
+(function () {
   var container = document.getElementById("container"),
     left = document.getElementById("left_panel"),
     right = document.getElementById("right_panel"),
     handle = document.getElementById("drag");
 
-  handle.onmousedown = function(e) {
+  handle.onmousedown = function (e) {
     isResizing = true;
   };
 
-  document.onmousemove = function(e) {
+  document.onmousemove = function (e) {
     // we don't want to do anything if we aren't resizing.
     if (!isResizing) {
       return;
     }
 
-    var offsetRight = container.clientWidth - (e.clientX - container.offsetLeft);
+    var offsetRight =
+      container.clientWidth - (e.clientX - container.offsetLeft);
 
     left.style.right = offsetRight + "px";
     right.style.width = offsetRight + "px";
-  }
+  };
 
-  document.onmouseup = function(e) {
+  document.onmouseup = function (e) {
     // stop resizing
     isResizing = false;
-  }
+  };
 })();
