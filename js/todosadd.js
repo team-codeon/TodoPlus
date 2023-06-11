ff479408377546685a25e4692ec1a9c922352422
Ricurrency <78289731+CaramelisedApples@users.noreply.github.com>
2023-06-11 20:58:48 +0800

    a

--- js/todosadd.js
+++ js/todosadd.js
@@ -1,21 +1,21 @@
 const alltodos = localStorage.getItem("todos").split(",");
 if (alltodos != "") {
-  alltodos.forEach(todoname => {
-    const box = `
-      <div class="todo">
-        <h4 class = "menutodostext">`+ todoname + `</h4>
-      </div>
-      
-      <div style = "height: 5px;"></div>
-      `;
-    document.getElementById("todos").innerHTML += box;
-  });
+alltodos.forEach(todoname => {
+  const box = `
+    <div class="todo">
+      <h4 class = "menutodostext">`+ todoname + `</h4>
+    </div>
+    
+    <div style = "height: 5px;"></div>
+    `;
+  document.getElementById("todos").innerHTML += box;
+});
 }
 
 const box = `
   <div class="addtodo" onclick = "addtodo()">
   <h4>Add todo</h4>
   </div>
-`
+`;
 
 document.getElementById("todos").innerHTML += box;
