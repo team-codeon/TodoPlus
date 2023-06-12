c0386cd0564e23737689ca5fd4149e7f1f4d26e2
Ricurrency <78289731+CaramelisedApples@users.noreply.github.com>
2023-06-12 15:13:05 +0800

    a

--- js/newtodoscript.js
+++ js/newtodoscript.js
@@ -2,10 +2,35 @@
 
 function addtodo() {
     if (alltodos != "") {
-        localStorage.setItem("todos",localStorage.getItem("todos")+","+"John Doe"/*Change to user choice of todo name*/)
+        localStorage.setItem("todos",localStorage.getItem("todos")+"John Doe"+","/*Change to user choice of todo name*/)
     }
 
     else {
-        localStorage.setItem("todos","John Doe")
+        localStorage.setItem("todos","John Doe"+",")
+    };
+
+    document.getElementById("todos").innerHTML = ""
+
+    const alltodos2 = localStorage.getItem("todos").split(",");
+    if (alltodos2 != "") {
+      alltodos2.forEach(todoname => {
+        if (todoname != "") {
+        const box = `
+          <div class="todo">
+            <h4 class = "menutodostext">`+ todoname + `</h4>
+          </div>
+          
+          <div style = "height: 5px;"></div>
+          `;
+        document.getElementById("todos").innerHTML += box;}
+      });
     }
+    
+    const box = `
+      <div class="addtodo" onclick = "addtodo()">
+      <h4>Add todo</h4>
+      </div>
+    `;
+    
+    document.getElementById("todos").innerHTML += box;
 }
--- js/todosadd.js
+++ js/todosadd.js
@@ -1,15 +1,17 @@
+document.getElementById("todos").innerHTML = ""
+
 const alltodos = localStorage.getItem("todos").split(",");
 if (alltodos != "") {
-alltodos.forEach(todoname => {
-  const box = `
-    <div class="todo">
-      <h4 class = "menutodostext">`+ todoname + `</h4>
-    </div>
-    
-    <div style = "height: 5px;"></div>
-    `;
-  document.getElementById("todos").innerHTML += box;
-});
+  alltodos.forEach(todoname => {
+    const box = `
+      <div class="todo">
+        <h4 class = "menutodostext">`+ todoname + `</h4>
+      </div>
+      
+      <div style = "height: 5px;"></div>
+      `;
+    document.getElementById("todos").innerHTML += box;
+  });
 }
 
 const box = `
