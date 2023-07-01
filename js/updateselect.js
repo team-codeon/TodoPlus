c3342e1daebde6dfa7a58855a9350f7bc8764386
Ricurrency <78289731+CaramelisedApples@users.noreply.github.com>
2023-07-01 14:45:13 +0800

    a

--- js/updateselect.js
+++ js/updateselect.js
@@ -1,11 +1,18 @@
 function updateselect(idnumber) {
-  document.getElementById(
+  // Update colour of Todo
+  if (localStorage.getItem("selectedid") != "null") {
+    document.getElementById(
     "todoitem" + localStorage.getItem("selectedid")
   ).style.backgroundColor = "#d4d4a6";
+  }
 
   localStorage.setItem("selectedid", idnumber);
 
   document.getElementById(
     "todoitem" + localStorage.getItem("selectedid")
   ).style.backgroundColor = "#999977";
+
+
+  // Update Todo name display
+  document.getElementById("tasksname").innerHTML = (localStorage.getItem("todos").split(","))[idnumber];
 }
