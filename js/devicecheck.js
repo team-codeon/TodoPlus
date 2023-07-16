2f7a6fc6fdee5a2a472a0345a681bbbc7f7d112a
SandwichedAvocado <ngweiyuan8@gmail.com>
2023-07-16 11:04:33 +0800

    Update devicecheck.js

--- js/devicecheck.js
+++ js/devicecheck.js
@@ -1,6 +1,6 @@
 // LocalStorage transfer to JSON completed
 
-/* Checks if device is a moblie or a desktop */
+// Checks if device is a moblie or a desktop
 if (
   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent
